fb1f3ec9e220262104f8bebb8ff1553362202597
wil974-dev <wggron99@gmail.com>
2025-10-02 16:13:05 +0400

    Mise en place du système de filtre, avec création de bouton dynamique

--- FrontEnd/Scripts/script.js
+++ FrontEnd/Scripts/script.js
@@ -8,7 +8,9 @@
  */
 
 const gallery = document.getElementsByClassName("gallery")[0];
+const filterContainer = document.getElementsByClassName("filter")[0];
 const apiGetWorks = "http://localhost:5678/api/works";
+const apiGetCategories = "http://localhost:5678/api/categories";
 
 /**
  * Retourne tous les travaux de l'architecte
@@ -18,6 +20,22 @@ const apiGetWorks = "http://localhost:5678/api/works";
 async function getWorks(){
     try{
         const response = await fetch(apiGetWorks);
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        console.error("Erreur :", error);
+    }
+}
+
+
+/**
+ * Demander au mentor si c'est bien de cette façon que l'on génère un JsDoc.
+ * @returns {Promise<Categories[]>} - Retourne une promesse contenant un tableau des catégories.
+ */
+async function getCategories(){
+    try{
+        const response = await fetch(apiGetCategories);
         const data = await response.json();
         console.log(data);
         return data;
@@ -33,11 +51,31 @@ async function getWorks(){
     console.log(Array.isArray(data));
 });*/
 
-async function createGallery(){
+/**
+ * Affichage dynamique des travaux 
+ */
+async function createGallery(category = "all"){
+    gallery.innerHTML = "";
     const works = await getWorks();
+    let worksFilter = [];
 
-    for(let i=0; i < works.length; i++){
-        const work = works[i];
+    if(category != "all"){
+        let workTemp = [];
+        for(let i = 0; i < works.length; i++){
+            if(works[i].category.name === category){
+                workTemp.push(works[i]);
+                worksFilter = [...workTemp];
+            }
+        }
+        
+    }
+    else{
+        worksFilter = [...works];
+    }
+
+    for(let i=0; i < worksFilter.length; i++){       
+        const work = worksFilter[i];
+        
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -52,4 +90,30 @@ async function createGallery(){
     }
 };
 
+/**
+ * Création dynamique des boutons du filtres à partir des catégories.
+ */
+async function createButtonCategories(){
+    const categories = await getCategories();
+
+    for(let i = 0; i < categories.length; i++){
+        const categorie = categories[i];
+        const button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.classList.add("button-filter");
+        button.innerText = categorie.name;
+        filterContainer.appendChild(button);
+        button.addEventListener("click", () => {createGallery(categorie.name)});
+    }
+}
+
+
+
+
+
+
+
+
+
 createGallery();
+createButtonCategories();
