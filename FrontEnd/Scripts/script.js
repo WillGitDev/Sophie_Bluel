43ba66e74b940bbd9954c4103e6b07c32c41c8d9
wil974-dev <wggron99@gmail.com>
2025-10-03 15:07:56 +0400

    Finalisation du trie en fonction de la catégorie

--- FrontEnd/Scripts/script.js
+++ FrontEnd/Scripts/script.js
@@ -25,19 +25,19 @@ async function getWorks(){
     }
     catch(error){
         console.error("Erreur :", error);
-    }
-}
-
+    };
+};
 
 /**
- * Demander au mentor si c'est bien de cette façon que l'on génère un JsDoc.
- * @returns {Promise<Categories[]>} - Retourne une promesse contenant un tableau des catégories.
+ * 
+ * @async
+ * @function getCategories
+ * @returns {Promise<Array<object>>} Retourne une promesse contenant un tableau des catégories.
  */
 async function getCategories(){
     try{
         const response = await fetch(apiGetCategories);
         const data = await response.json();
-        console.log(data);
         return data;
     }
     catch(error){
@@ -52,26 +52,34 @@ async function getCategories(){
 });*/
 
 /**
- * Affichage dynamique des travaux 
+ * Affiche dynamiquement les travaux. Si aucun paramètre de catégorie n'est
+ * spécifié, tous les travaux sont affichés.
+ * 
+ * @async
+ * @function createGallery
+ * @param {string} [category="all"] - Nom de la catégorie à filtrer. Si aucune valeur n'est fournie tous les travaux sont affichés.
+ * @returns {Promise<void>} Ne retourne rien, mais modifie le DOM.
  */
 async function createGallery(category = "all"){
     gallery.innerHTML = "";
     const works = await getWorks();
     let worksFilter = [];
 
-    if(category != "all"){
+    if(category !== "all"){
         let workTemp = [];
         for(let i = 0; i < works.length; i++){
             if(works[i].category.name === category){
-                workTemp.push(works[i]);
-                worksFilter = [...workTemp];
-            }
-        }
+                /*workTemp.push(works[i]);
+                worksFilter = [...workTemp];*/
+                worksFilter.push(works[i]);
+                
+            };
+        };
         
     }
     else{
         worksFilter = [...works];
-    }
+    };
 
     for(let i=0; i < worksFilter.length; i++){       
         const work = worksFilter[i];
@@ -82,19 +90,34 @@ async function createGallery(category = "all"){
 
         img.src = work.imageUrl;
         img.alt = work.title;
-        figcaption.innerText = work.title;
+        figcaption.textContent = work.title;
 
         figure.appendChild(img);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
-    }
+    };
 };
 
 /**
- * Création dynamique des boutons du filtres à partir des catégories.
+ * Création dynamique des boutons des filtres à partir des catégories.
+ * 
+ * @async
+ * @function createButtonCategories
+ * @return {Promise<void>} ne retourne rien mais modifie le DOM.
  */
 async function createButtonCategories(){
     const categories = await getCategories();
+    const button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.classList.add("button-filter");
+    button.innerText = "Tous";
+    filterContainer.appendChild(button);
+    button.classList.add("button-active");
+    button.addEventListener("click", (event)=>{
+        classActiveButton(event);
+        createGallery("all");
+
+    });
 
     for(let i = 0; i < categories.length; i++){
         const categorie = categories[i];
@@ -103,17 +126,29 @@ async function createButtonCategories(){
         button.classList.add("button-filter");
         button.innerText = categorie.name;
         filterContainer.appendChild(button);
-        button.addEventListener("click", () => {createGallery(categorie.name)});
-    }
+        button.addEventListener("click", (event)=> {
+            classActiveButton(event);
+            createGallery(categorie.name)
+        });
+        
+    };
+};
+
+/**
+ * Permet d'effacer la class button-active sur tous les boutons du filtre
+ * et d'appliquer cette même classe uniquement sur le bouton sélectionner.
+ * @param {event} event 
+ */
+function classActiveButton(event){
+    const buttonFilter = document.querySelectorAll(".button-filter");
+    buttonFilter.forEach((button) => {
+        button.classList.remove("button-active");
+    });
+    event.target.classList.add("button-active");
 }
-
-
-
-
-
-
-
 
 
 createGallery();
 createButtonCategories();
+
+
